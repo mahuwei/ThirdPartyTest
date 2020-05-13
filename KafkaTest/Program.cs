0908ee6f9a7dcafd2cd3a2c004998ebaaabe9ec0
mahuwei <mahuwei@msn.com>
2020-05-13 15:07:44 +0800

    添加Kafka测试。

---
+++ KafkaTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
